a80965b99d4269da12f2328420f8a18c2fc6a0bd
Robert Voss Junior <vossrobertt@gmail.com>
2022-11-22 16:41:15 -0300

    random-button

--- guessthecolor-game/src/App.jsx
+++ guessthecolor-game/src/App.jsx
@@ -6,26 +6,35 @@ import "./Style.css"
 function App() {
 
     const [color, setColor] = useState("");
+    const [hexcerto, setHexcerto] = useState("");
 
     const getRandomColor = () => {
         var letters = '0123456789ABCDEF';
         var color = '#';
         for (var i = 0; i < 6; i++) {
-          color += letters[Math.floor(Math.random() * 16)];
+            color += letters[Math.floor(Math.random() * 16)];
         }
-
         return color;
-      }
+    }
 
+    const getHexCerto = () => {
+        var hexcerto = {color};
+        console.log({hexcerto})
+        
+        return hexcerto;
+    }
     useEffect(() => {
-        setColor(getRandomColor())
+        const actualColor = getRandomColor();
+        setColor(actualColor);
     }, []);
 
     return <div className="App">
         <div className="guess"
-        style={{ background:color }}>
-
+            style={{ background: color }}>
         </div>
+        <button>asdasdsdada</button>
+        <button>asdasdasdda</button>
+        <button>aasdsadadsa</button>
     </div>
 }
 
