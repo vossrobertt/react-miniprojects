fdf6360599e5ab563606acf9cf1fcac205a66364
robertvoss <vossrobertt@gmail.com>
2022-11-22 20:07:50 -0300

    gameworking-without-changingbuttons

--- guessthecolor-game/src/App.jsx
+++ guessthecolor-game/src/App.jsx
@@ -2,39 +2,54 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import "./Style.css"
 
+const getRandomColor = () => {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 function App() {
 
     const [color, setColor] = useState("");
-    const [hexcerto, setHexcerto] = useState("");
+    const [hexcerto, setHexcerto] = useState([]);
 
-    const getRandomColor = () => {
-        var letters = '0123456789ABCDEF';
-        var color = '#';
-        for (var i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-    }
+    const [result, setResult] = useState(undefined);
 
-    const getHexCerto = () => {
-        var hexcerto = {color};
-        console.log({hexcerto})
-        
-        return hexcerto;
-    }
-    useEffect(() => {
+    const generateColor = () => {
         const actualColor = getRandomColor();
         setColor(actualColor);
+        setHexcerto([actualColor, getRandomColor(), getRandomColor()].sort(() => 0.5 < Math.random()));
+    }
+
+    useEffect(() => {
+        generateColor();
     }, []);
 
+    const handleHexClick = (hexcerto) => {
+        if(hexcerto === color){
+            setResult(true);
+            generateColor();
+        } else {
+            setResult(false);
+        }
+    }
+
     return <div className="App">
-        <div className="guess"
-            style={{ background: color }}>
+        <div className="col">
+            
+            <div className="guess" style={{ background: color }}></div>
+            {hexcerto.map(hexcerto => (
+                <button 
+                onClick={() => handleHexClick(hexcerto)}
+                key={hexcerto}>{hexcerto}</button>
+            ))}
+
+            {result === false && <div className="errada">Resposta Errada!</div>}
+            {result === true && <div className="correta">Resposta Certa!</div>}
         </div>
-        <button>asdasdsdada</button>
-        <button>asdasdasdda</button>
-        <button>aasdsadadsa</button>
     </div>
 }
 
